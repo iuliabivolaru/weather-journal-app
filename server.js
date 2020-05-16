a493feb98a5d2257614dcb24a1e2e935655a59b5
Iulia Bivolaru <iulia.bivolaru10@gmail.com>
2020-05-16 19:52:46 +0200

    Feat: setup the server; implement the post method on the client and server sides

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+var projectData = {};
+receivedDataArray = [];
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -12,9 +17,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+const server = app.listen(port, () => console.log(`server up and running on localhost:${port}`));
+
+// GET and POST route handlers
+app.get('/all', function getHandler(request, response) {
+    response.send(projectData);
+});
+
+app.post('/data', function postHandler(request, response) {
+    let body = request.body;
+    let weatherData = {
+        temperature: body.temperature,
+        date: body.date,
+        userReponse: body.userReponse
+    };
+    receivedDataArray.push(weatherData);
+});
--- website/app.js
+++ website/app.js
@@ -1,4 +1,24 @@
 /* Global Variables */
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=2ce7c639a23cd64116b67d37126b24dd';
+
+document.getElementById('generate').addEventListener('click', populateAndGetWeatherData);
+
+function populateAndGetWeatherData() {
+    const zipCode = document.getElementById('zip').value;
+    getWeatherData(baseUrl, zipCode, apiKey);
+}
+
+const getWeatherData = async (baseUrl, zipCode, apiKey) => {
+    const response = await fetch(baseUrl + zipCode + apiKey);
+    try {
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch(error) {
+        console.log(error);
+    }
+}
 
 // Create a new date instance dynamically with JS
 let d = new Date();
